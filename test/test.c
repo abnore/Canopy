78eafb4e908ed5a7ffae85d34d10ce59a14d9a57
Andreas Bjørgulv Nore <andrebn@ulrik.uio.no>
2025-03-25 01:24:46 +0100

    added to .gitignore

--- test/test.c
+++ test/test.c
@@ -5,19 +5,19 @@
 
 int main() {
     canopy_window* win = canopy_create_window(800, 600, "Hello Canopy");
-    BMP *bmp_tiles = picasso_load_bmp("background.bmp");
-
-    if (!bmp_tiles) {
+    BMP *bmp = picasso_load_bmp("background.bmp");
+    if (!bmp) {
         fprintf(stderr, "Failed to load BMP\n");
         return 1;
     }
 
-    printf("Loaded BMP: %dx%d\n", bmp_tiles->ih.width, bmp_tiles->ih.height);
-    picasso_flip_buffer_vertical(bmp_tiles->image_data, bmp_tiles->ih.width, bmp_tiles->ih.height);
+    printf("Loaded BMP: %dx%d\n", bmp->ih.width, bmp->ih.height);
+    picasso_flip_buffer_vertical(bmp->image_data, bmp->ih.width, bmp->ih.height);
 
-    canopy_render_bitmap(win, bmp_tiles->image_data,
-                         bmp_tiles->ih.width, bmp_tiles->ih.height,
+    canopy_render_bitmap(win, bmp->image_data,
+                         bmp->ih.width, bmp->ih.height,
                          0, 0);
+
 
     while (!canopy_window_should_close(win)) {
         canopy_redraw_buffer(win);
@@ -40,8 +40,8 @@ int main() {
     }
 
     canopy_destroy_window(win);
-    free(bmp_tiles->image_data);
-    free(bmp_tiles);
+    free(bmp->image_data);
+    free(bmp);
 
     return 0;
 }
