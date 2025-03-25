af661f4830dc1983151fd61b4fb50f42d1dc119f
Andreas Bjørgulv Nore <andrebn@ulrik.uio.no>
2025-03-25 01:12:54 +0100

    adding a test file

---
+++ test/test.c
@@ -0,0 +1,48 @@
+#include "canopy.h"
+#include "picasso.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    canopy_window* win = canopy_create_window(800, 600, "Hello Canopy");
+    BMP *bmp_tiles = picasso_load_bmp("background.bmp");
+
+    if (!bmp_tiles) {
+        fprintf(stderr, "Failed to load BMP\n");
+        return 1;
+    }
+
+    printf("Loaded BMP: %dx%d\n", bmp_tiles->ih.width, bmp_tiles->ih.height);
+    picasso_flip_buffer_vertical(bmp_tiles->image_data, bmp_tiles->ih.width, bmp_tiles->ih.height);
+
+    canopy_render_bitmap(win, bmp_tiles->image_data,
+                         bmp_tiles->ih.width, bmp_tiles->ih.height,
+                         0, 0);
+
+    while (!canopy_window_should_close(win)) {
+        canopy_redraw_buffer(win);
+
+        canopy_event event;
+        while (canopy_poll_event(&event)) {
+            switch (event.type) {
+            case CANOPY_EVENT_MOUSE_DOWN:
+                printf("mouse down\n");
+                break;
+            case CANOPY_EVENT_MOUSE_UP:
+                printf("mouse up\n");
+                break;
+            case CANOPY_EVENT_MOUSE_MOVE:
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    canopy_destroy_window(win);
+    free(bmp_tiles->image_data);
+    free(bmp_tiles);
+
+    return 0;
+}
+
