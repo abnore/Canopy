d3c2d0fc6a0203c680c4733a6c35f3fd54c1ce8c
Andreas Bjørgulv Nore <andrebn@ulrik.uio.no>
2025-03-28 06:25:55 +0100

    added test file - will clean up later

--- test/test.c
+++ test/test.c
@@ -1,12 +1,90 @@
+/*******************************************************************************************
+*
+*   CANOPY [Example] - Custom Framebuffer Rendering
+*
+*   Description:
+*       Renders a solid-colored rectangle using a manually created framebuffer.
+*       Demonstrates how to allocate, fill, and display a custom framebuffer using Canopy.
+*
+*   Controls:
+*       [Close Window] - Exit application
+*
+********************************************************************************************/
+
 #include "canopy.h"
-#include "logger.h"
-#include "picasso.h"
+
+#define WIDTH   400
+#define HEIGHT  400
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    if (!init_log(NULL, true)) {
+        printf("Failed to initialize logger\n");
+        return 1;
+    }
+
+    canopy_window* win = canopy_create_window(WIDTH, HEIGHT, "Canopy - Custom Framebuffer");
+    canopy_set_buffer_refresh_color(win, CANOPY_DARK_GRAY);
+
+    canopy_init_timer();
+    canopy_set_fps(60);
+
+    framebuffer fb;
+    fb.width = 200;
+    fb.height = 200;
+    fb.pitch = fb.width * CANOPY_BYTES_PER_PIXEL;
+    fb.clear_color = CANOPY_BLUE;
+
+    size_t buffer_size = fb.pitch * fb.height;
+    fb.pixels = canopy_malloc(buffer_size);
+
+    if (!fb.pixels) {
+        FATAL("Failed to allocate framebuffer");
+        return 1;
+    }
+
+    // Fill the framebuffer with its clear color (once)
+    for (int i = 0; i < fb.width * fb.height; ++i) {
+        fb.pixels[i] = color_to_u32(fb.clear_color);
+    }
+    //--------------------------------------------------------------------------------------
+
+    // Main Game Loop
+    while (!canopy_window_should_close(win))
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        canopy_event event;
+        while (canopy_poll_event(&event)) {
+            // Handle events (mouse, keyboard, etc.)
+        }
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        if (canopy_should_render_frame()) {
+            canopy_clear_buffer(win);                    // Clear main window framebuffer
+            canopy_swap_backbuffer(win, &fb);            // Copy custom framebuffer
+            canopy_present_buffer(win);                  // Present on screen
+        }
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    canopy_free(fb.pixels);
+    canopy_free_window(win);
+    shutdown_log();
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
 
 
-#define WIDTH 800
-#define HEIGHT 600
-
-#if 0
+/*
+#if 1
 int main(void)
 {
     if (init_log(NULL, true) == LOG_ERROR)
@@ -24,17 +102,11 @@ int main(void)
     color rect1[200 * 200];
     color rect2[200 * 200];
     color rect3[200 * 200];
-    color rect4[200 * 200];
-    color rect5[200 * 200];
-    color rect6[200 * 200];
 
     // Fill the rectangles with colors
     picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect1 }, CANOPY_RED);
-    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect2 }, (color){0, 0, 200, 128});
-    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect3 }, (color){0, 200, 0, 170});
-    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect4 }, CANOPY_PURPLE);
-    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect5 }, CANOPY_NAVY);
-    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect6 }, CANOPY_GOLD);
+    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect2 }, CANOPY_NAVY);
+    picasso_fill_backbuffer(&(picasso_backbuffer){ .width = 200, .height = 200, .pixels = (uint32_t*)rect3 }, CANOPY_GOLD);
 
     canopy_init_timer();
     canopy_set_fps(60);
@@ -52,14 +124,11 @@ int main(void)
 
         if (canopy_should_render_frame()) {
             canopy_clear_buffer(win);
+
             picasso_clear_backbuffer(bf);
-            // Blit the colored rectangles to the backbuffer
             picasso_blit_bitmap(bf, rect1, 200, 200, 100, 100 );
-            picasso_blit_bitmap(bf, rect2, 200, 200, 150, 150 );
-            picasso_blit_bitmap(bf, rect3, 200, 200, 200, 200 );
-            picasso_blit_bitmap(bf, rect4, 200, 200, 300, 100 );
-            picasso_blit_bitmap(bf, rect5, 200, 200, 350, 150 );
-            picasso_blit_bitmap(bf, rect6, 200, 200, 400, 200 );
+            picasso_blit_bitmap(bf, rect2, 200, 200, 350, 150 );
+            picasso_blit_bitmap(bf, rect3, 200, 200, 400, 200 );
 
             canopy_swap_backbuffer(win, (framebuffer*)bf);
             canopy_present_buffer(win);
@@ -185,3 +254,8 @@ int main(void)
     return 0;
 }
 #endif
+*/
+
+
+
+
